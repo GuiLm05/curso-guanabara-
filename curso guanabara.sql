d8ad0b2b8b3f27e95e69a7b353560657b90cc6b1
GuiLm05 <guilimagui2005@gmail.com>
2024-09-01 23:02:11 -0300

    Update curso guanabara.sql

--- curso guanabara.sql
+++ curso guanabara.sql
@@ -34,3 +34,25 @@ nome varchar(30) not null,
  totaulas int unsigned,
  ano year default '2024'
  ) default charset = utf8;
+use cadastro;
+ alter table cursos add column idcurso int first;
+ alter table cursos add primary key (idcurso);
+ insert into cursos values
+('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
+ ('2','Algoritimos','Logica de Programação','20','8','2014'),
+ ('3','Photoshop','Aulas de Photoshop CC','9','20','2014'),
+ ('4','PGP','PHP para Iniciantes','33','40','2010'),
+ ('5','Jarva','Intro ao Java','22','10','2000'),
+ ('6','MySQL','Curso MySQL','21','15','2016'),
+ ('7','World','Word Completo','40','30','2018'),
+ ('8','Sapateado','Dança Rítimica','14','30','2018'),
+ ('9','Cozinha Árabe','Aprenda a fazer Kibe','40','30','2018'),
+ ('10','YouTuber','Gerar Polêmicas e Ganhar Inscritos','5','2','2010');
+ update cursos 
+ set nome = 'java', carga ='40', ano = '2015'
+ where idcurso = '5'
+ limit 1;
+ delete from cursos 
+ where idcurso = '10';
+select * from cursos;
+ 
